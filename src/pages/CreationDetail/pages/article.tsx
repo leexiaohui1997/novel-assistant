4456f9e43fcd26cecceae242a03227e29ce5a07c
leexiaohui1997 <1287338537@qq.com>
2026-05-07 16:52:05 +0800

    refactor(creation-detail): 将章节管理迁移到文章页
    - basic.tsx 仅保留基本信息卡片，职责更聚焦
    - article.tsx 承载章节管理卡片，符合"文章=章节"的页面语义

--- src/pages/CreationDetail/pages/article.tsx
+++ src/pages/CreationDetail/pages/article.tsx
@@ -1,7 +1,16 @@
+import { Card } from 'antd'
 import React from 'react'
 
+import ChapterManagement from '../components/ChapterManagement'
+
 const CreationDetailArticle: React.FC = () => {
-  return <div>CreationDetailArticle</div>
+  return (
+    <div className="p-6! flex flex-col gap-4">
+      <Card title="章节管理">
+        <ChapterManagement />
+      </Card>
+    </div>
+  )
 }
 
 export default CreationDetailArticle
--- src/pages/CreationDetail/pages/basic.tsx
+++ src/pages/CreationDetail/pages/basic.tsx
@@ -1,16 +1,11 @@
-import { Card } from 'antd'
 import React from 'react'
 
 import BasicInfoCard from '../components/BasicInfoCard'
-import ChapterManagement from '../components/ChapterManagement'
 
 const CreationDetailBasic: React.FC = () => {
   return (
     <div className="p-6! flex flex-col gap-4">
       <BasicInfoCard />
-      <Card title="章节管理">
-        <ChapterManagement />
-      </Card>
     </div>
   )
 }
